b5a0c8ad5c7a995e3b08b7cca6bff8a0129a42dd
代圣达 <daishengda@hinterstellar.com>
2018-11-12 20:42:23 +0800

    update READEME.mx

---
+++ app/src/main/java/com/darius/android/activities/ActivitiesDemoActivity.java
@@ -0,0 +1,18 @@
+package com.darius.android.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.darius.android.R;
+
+/**
+ * 所有Activity相关Demo入口
+ */
+public class ActivitiesDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiyties_demo_activity);
+    }
+}
